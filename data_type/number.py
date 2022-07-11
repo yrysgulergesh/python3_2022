ed7e7a869e5cefe9825ca159ac5c0fb91f6865c0
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-07-12 01:19:17 +0600

    add number exzamples

---
+++ data_type/number.py
@@ -0,0 +1,46 @@
+num = 3
+print(type(num))
+
+num_1 = 7
+num_2 = 9
+result = num_1 + num_2
+
+print(result)
+
+num_1 = 5
+num_2 = 10
+result = -(num_1 - num_2)
+
+print(result)
+
+num_1 = 5
+num_2 = 10
+result = num_1 * num_2
+
+print(result)
+
+num_1 = 5
+num_2 = 10
+result = (num_1 * num_2) * 2
+
+print(result)
+# print(result*2)
+
+num_3 = 8 ** 3
+print(num_3)
+
+num_4 = 50 / 8
+print(num_4)
+
+num_5 = 50 / 7
+print(int(num_5))
+
+num_6 = 50 // 6
+print(num_6)
+
+num_7 = 50 % 6
+print(num_7)
+
+num_8 = 9
+num_9 = 5
+print(num_8 > num_9)
