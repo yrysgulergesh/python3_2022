9aac0804609af2743a4bdf8e0fb85258d6791585
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-07-11 19:09:03 +0600

    data type python

--- 1-less/hello.py
+++ 1-less/hello.py
@@ -1,3 +1,3 @@
 print('Hello World!')
 print('Hello, Python!')
-print('Привет дорогие друзья!')
+print("Привет 'дорогие' друзья!")
---
+++ data_type/data_type.py
@@ -0,0 +1,26 @@
+# Data Type
+# int - Целостные числа 5, 10, 200
+# float - числа с плавающей запятой 5.5, 10.2
+# str - строки 'hello'
+# boolean - True, False
+
+# var
+# _var
+# 123var, !var -ошибка
+
+
+# azsdfg ADDEFDF 0123456789
+# Var - ошибка
+# vAr
+# var123
+
+# first_name
+# firstName - ошибка
+# first name, !@#$* -var! - ошибка
+#
+# var
+
+num_1 = 2
+num_2 = 5
+result = num_1 + num_2
+print(result)
